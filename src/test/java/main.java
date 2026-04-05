2003abf308a252f8222f3a416a8bfc5025460e33
legion <meleshkoilya49@gmail.com>
2026-04-05 23:44:00 +0300

    qa-guru

---
+++ src/test/java/main.java
@@ -0,0 +1,10 @@
+ppackage guru.qa;
+
+public class Main {
+
+    private static String msg = "Hello qa.guru!";
+
+    public static void main(String... args) {
+        System.out.println(msg);
+    }
+}
